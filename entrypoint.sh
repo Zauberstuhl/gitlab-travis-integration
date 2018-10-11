111a65de8fa9a4fb8b38b6c539fdc3b2a663901c
Lukas Matt <lukas@matt.wf>
2018-10-11 23:27:43 +0200

    Use deep_merge in travis request

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 max_duration=$((45 * 60))
 starting_time=$(date +%s)
 ending_time=$(($starting_time + $max_duration))
-travis_config='"config":{"env":{"matrix":["WORKER=true"]}}'
+travis_config='"config":{"merge_mode":"deep_merge","env":{"WORKER":"true"}}'
 encoded_slug=$(echo $TRAVIS_REPO_SLUG |sed 's/\//%2F/g')
 
 function check_gitlab() {

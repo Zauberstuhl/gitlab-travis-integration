a8b152933c2428a9eeb98a3eaabffaf0295c5ddc
Lukas Matt <lukas@matt.wf>
2018-10-12 22:16:32 +0200

    Extract agent and worker code

---
+++ agent.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# https://docs.travis-ci.com/user/customizing-the-build#Build-Timeouts
+max_duration=$((45 * 60))
+starting_time=$(date +%s)
+ending_time=$(($starting_time + $max_duration))
+travis_config='"config":{"script":"bash entrypoint.sh WORKER"}'
+encoded_slug=$(echo $TRAVIS_REPO_SLUG |sed 's/\//%2F/g')
+
+function check_gitlab() {
+  page=0
+  api=${gitlaburl}/api/v4
+  header="Private-Token: $gitlabauthtoken"
+  while true; do
+    project_cnt=0
+    for id in $(curl -s "$api/projects?page=$page" | jq '.[].id'); do
+      api_jobs="$api/projects/$id/jobs?scope=pending"
+      pending=$(curl -s --header "$header" "$api_jobs" | jq '.[]')
+      if [[ "$pending" != "" ]] && [[ ! -f /tmp/gti.${id}.lock ]]; then
+        touch /tmp/gti.${id}.lock
+        echo 1
+        return
+      fi
+      project_cnt=$(( $project_cnt + 1 ))
+    done
+    if [ $project_cnt -eq 0 ]; then
+      echo 0
+      return
+    fi
+    page=$(( $page + 1 ))
+  done
+  echo 0
+  return
+}
+
+while [[ $(date +%s) -lt $ending_time ]];
+do
+  if [ $(check_gitlab) -eq 1 ]; then
+    # start worker process
+    curl -s -X POST \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/json" \
+      -H "Travis-API-Version: 3" \
+      -H "Authorization: token $travistoken" \
+      -d "{\"request\": {\"branch\":\"${TRAVIS_BRANCH}\",$travis_config}}" \
+      https://api.travis-ci.org/repo/${encoded_slug}/requests
+  fi
+  # some kind of output is required otherwise
+  # travis will cancel the job earlier
+  echo -n "."
+  sleep 10
+done
+
+# restart agent process
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token $travistoken" \
+  -d "{\"request\": {\"branch\":\"${TRAVIS_BRANCH}\"}}" \
+  https://api.travis-ci.org/repo/${encoded_slug}/requests
---
+++ worker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+project_id=$1
+
+docker pull gitlab/gitlab-runner:latest
+docker run --name gitlab-runner --privileged \
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  -v "$(pwd)/config:/etc/gitlab-runner" \
+  -d gitlab/gitlab-runner:latest
+sed -i "s#TOKEN#${gitlabtoken}#" config/config.toml
+sed -i "s#URL#${gitlaburl}#" config/config.toml
+
+while true; do
+  started=$(docker logs gitlab-runner 2>&1 |grep 'received' |wc -l)
+  finished=$(docker logs gitlab-runner 2>&1 |grep 'Job succeeded' |wc -l)
+  if [ $started -eq $finished ] && [ $started -ne 0 ]; then break; fi
+  # some kind of output is required otherwise
+  # travis will cancel the job earlier
+  echo -n "."
+  sleep 30
+done
+
+rm /tmp/gti.${project_id}.lock || true

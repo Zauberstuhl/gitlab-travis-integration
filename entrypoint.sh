0ca2c491f0f97c15a66444889a7d45923cd79409
Lukas Matt <lukas@matt.wf>
2019-08-05 23:46:49 +0200

    More simple is even better

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == "WORKER" ]]; then
-  echo -n "Executing worker" && bash worker.sh $2
-else
-  echo -n "Executing agent" && bash agent.sh
-fi
--- worker.sh
+++ worker.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+while true; do
+  # some kind of output is required otherwise
+  # travis will cancel the job earlier
+  echo -n "."
+  sleep 1
+done &
+
+# configure runner
 sed -i "s#TOKEN#${gitlabtoken}#" config/config.toml
 sed -i "s#URL#${gitlaburl}#" config/config.toml
 
+# start runner
 docker pull gitlab/gitlab-runner:latest
 docker run --name gitlab-runner --privileged \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
@@ -11,3 +20,15 @@ docker run --name gitlab-runner --privileged \
 
 # https://docs.travis-ci.com/user/customizing-the-build#Build-Timeouts
 sleep $(( 45 * 60 ))
+
+# restart process
+encoded_slug=$(echo $TRAVIS_REPO_SLUG |sed 's/\//%2F/g')
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token $travistoken" \
+  -d "{\"request\": {\"branch\":\"${TRAVIS_BRANCH}\"}}" \
+  https://api.travis-ci.org/repo/${encoded_slug}/requests
+
+exit 0
